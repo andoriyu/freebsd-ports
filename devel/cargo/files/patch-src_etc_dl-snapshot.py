200ac038b278893a458b0c8a723ff664a52376bd
Andrey Cherkashin <with.out@me.com>
2015-06-27 22:17:57 -0700

    [FIX][devel/cargo] wtf wtf (the same wtf, just forgot the file)

---
+++ devel/cargo/files/patch-src_etc_dl-snapshot.py
@@ -0,0 +1,8 @@
+--- src/etc/dl-snapshot.py.orig	2015-06-28 05:16:30 UTC
++++ src/etc/dl-snapshot.py
+@@ -1,3 +1,5 @@
++#!/usr/local/bin/python
++
+ import distutils.spawn
+ import hashlib
+ import os
