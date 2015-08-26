e4f8e4df06d8b5392abde73fc46232a0bc73ace8
Andrey Cherkashin <with.out@me.com>
2015-08-26 11:30:06 -0700

    Update cargo, move thigns around in rust ports

--- devel/cargo/files/patch-src_etc_dl-snapshot.py
+++
@@ -1,8 +0,0 @@
---- src/etc/dl-snapshot.py.orig	2015-06-26 23:23:44 UTC
-+++ src/etc/dl-snapshot.py
-@@ -1,3 +1,5 @@
-+#!/usr/local/bin/python2
-+
- import distutils.spawn
- import hashlib
- import os
